bcb98df4c7e170899da2a05f1daaf30e6d2fc8c9
wonsss <bigdatajang@gmail.com>
2021-08-27 00:52:31 +0900

    replaceTrack

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -97,6 +97,13 @@ function handleConnectClick() {
 
 async function handleCameraChange() {
   await getMedia(camerasSelect.value);
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
 function handleFullscreen() {
